ac8e0ae3712ce9da31fc5a25535d7b0546de26d3
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-14 02:12:52 -0800

    add route paths

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <About />
-      <Shop />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+      </div>
+    </Router>
   );
 }
 
