ec2beb52808335cef38c29577d17776392ac1fcf
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-14 02:30:08 -0800

    add fortnight api

--- my-app/src/Shop.js
+++ my-app/src/Shop.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Shop() {
+
+    useEffect(() => {
+        fetchItems()
+    }, []);
+
+    const fetchItems = async () => {
+        const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get')
+        const items = await data.json();
+        console.log(items)
+    }
     return (
         <div className="App">
             <h1>Shop Page</h1>
